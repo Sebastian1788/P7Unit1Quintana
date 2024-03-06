4b92d67bf12551ca94adb6ac0862d383557a696f
Sebastian1788 <145072446+Sebastian1788@users.noreply.github.com>
2024-03-05 20:28:26 -0800

    Unity
    I made my truck move forward, backwards, and made it go side by side by coding.

--- Unit1QuintanaP7/Assets/Scripts/PlayerController.cs
+++ Unit1QuintanaP7/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float turnspeed;
+    public float horizontalInput;
+    public float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Verticle");
+
+        // We'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horizontalInput);
     }
 }
